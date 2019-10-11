3f3b754e803c838101c31b09b4aa73a5f3a3ccde
dilpreet kaur <dilpreet.kukreja@gmail.com>
2019-10-11 09:31:31 -0700

    Updates

---
+++ precache-manifest.21bd2d38bd1e41ad6e785671190d4454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a732a582ff0f8ed6997627e61c388ff",
+    "url": "/ToDoListApp-using-React/index.html"
+  },
+  {
+    "revision": "bcf8d439473500f2e0fa",
+    "url": "/ToDoListApp-using-React/static/css/main.5e9f4692.chunk.css"
+  },
+  {
+    "revision": "f8adce70fe985fd73877",
+    "url": "/ToDoListApp-using-React/static/js/2.b253bd3c.chunk.js"
+  },
+  {
+    "revision": "bcf8d439473500f2e0fa",
+    "url": "/ToDoListApp-using-React/static/js/main.c67d2c3d.chunk.js"
+  },
+  {
+    "revision": "e40574bb27fb146dd9db",
+    "url": "/ToDoListApp-using-React/static/js/runtime-main.38e46720.js"
+  }
+]);
